93def833e90824921ee0c961552769ea2fad3f22
Erkan00 <erkandogan03423@gmail.com>
2025-10-17 15:52:46 +0300

    Create HesapMakinesi.cpp

---
+++ HesapMakinesi.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+ 
+int main() { 
+
+char op;
+double num1;
+double num2;
+double result;
+
+cout<<"**********Hesap Makinesi**********\n";
+
+
+cout<<"isleminiz(+,-,*,/):";
+cin>>op;
+
+cout<<"birinci sayiyi giriniz:";
+cin>>num1;
+
+cout<<"ikinci sayiyi giriniz:";
+cin>>num2;
+
+switch(op){
+  case  '+':
+  result= num1+num2;
+  cout<<"Cevap:"<< result << '\n';
+  break;
+
+  case  '-':
+  result= num1-num2;
+  cout<<"Cevap:"<< result << '\n';
+  break;
+
+  case  '*':
+  result= num1*num2;
+  cout<<"Cevap:"<< result << '\n';
+  break;
+
+ case  '/':
+  result= num1/num2;
+  cout<<"Cevap:"<< result << '\n';
+  break;
+  default:
+  cout<< "Bu geçerli bir sebep değil\n";
+  break;
+}
+
+cout<<"**************************************";
+
+
+return 0;
+
+}
